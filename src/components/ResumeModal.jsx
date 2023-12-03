f76cce2bd01bcc28ab0ea8b8f8d4924b7189ead7
nawafilhusnul <nawafilhusnul@gmail.com>
2023-12-04 00:55:12 +0700

    fix: adjust text color

--- src/components/ResumeModal.jsx
+++ src/components/ResumeModal.jsx
@@ -1,3 +1,5 @@
+import { AiOutlineFilePdf } from "react-icons/ai";
+
 export function ModalResume() {
   return (
     <dialog id="resume_modal" className="modal">
@@ -5,19 +7,21 @@ export function ModalResume() {
         <h3 className="font-bold text-lg dark:text-gray-300 py-4">Which file do you want to download?</h3>
         <div className="flex py-6 flex-wrap justify-between gap-4 px-6 select-none">
           <button
-            className="flex-1 mx-4 hover:bg-gradient-to-r from-pink-500  to-red-500 dark:text-white py-2 rounded-md text-sm border border-pink-500"
+            className="flex-1 flex justify-center font-semibold items-center gap-2 mx-4 hover:bg-gradient-to-r from-pink-500  to-red-500 dark:text-white hover:text-white py-2 rounded-md text-sm border border-pink-500"
             onClick={()=>{
               window.open('https://firebasestorage.googleapis.com/v0/b/me-dashboard-4830a.appspot.com/o/Resume%20Husnul%201.pdf?alt=media');
             }}
           >
+            <AiOutlineFilePdf className="text-2xl text-red-700"/>
             PDF
           </button>
           <button
-            className="flex-1 mx-4 hover:bg-gradient-to-r from-cyan-500  to-blue-500 dark:text-white py-2 rounded-md text-sm border border-cyan-500"
+            className="flex-1 flex justify-center font-semibold items-center gap-2 mx-4 text- hover:bg-gradient-to-r from-cyan-500  to-blue-500 dark:text-white hover:text-white py-2 rounded-md text-sm border border-cyan-500"
             onClick={()=>{
               window.open('https://firebasestorage.googleapis.com/v0/b/me-dashboard-4830a.appspot.com/o/Resume%20Husnul%201.docx?alt=media');
             }}
           >
+            <AiOutlineFilePdf className="text-2xl text-blue-700"/>
             WORD
           </button>
         </div>
