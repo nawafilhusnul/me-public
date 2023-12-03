604a06dc9a88675eaef307b49bfcbd8cb3b28868
nawafilhusnul <nawafilhusnul@gmail.com>
2023-12-03 19:37:36 +0700

    fix: adjust h1 heading to items center

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 export default function Navbar({ darkMode , setDarkMode }) {
   return (
     <>
-    <nav className="py-10 mb-12 flex justify-between select-none">
+    <nav className="py-10 mb-12 flex justify-between select-none items-center">
       <h1 className="text-xs xs:text-xl font-mono dark:text-gray-200">nawafilhusnul.com</h1>
       <ul className="flex items-center">
         <li><BsFillMoonStarsFill onClick={()=>setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-gray-300"/></li>
