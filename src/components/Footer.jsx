794a8e166d210d4b1bff2f9482140e2794d6a5c7
nawafilhusnul <nawafilhusnul@gmail.com>
2023-12-03 20:02:36 +0700

    fix: adjust footer padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer footer-center p-4 text-gray-600">
+    <footer className="footer footer-center px-4 pt-16 pb-8 text-gray-600">
       <aside>
         <p>Copyright © 2023 - All right reserved by Husnul Nawafil</p>
       </aside>
