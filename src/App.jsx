8011650115d4ba424a421ee688a770745b4897d0
nawafilhusnul <nawafilhusnul@gmail.com>
2023-12-07 10:03:15 +0700

    fix: adjust footer position

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,16 @@ function App() {
   const [ darkMode, setDarkMode ] = useState(true);
   return (
     <div className={`${darkMode ? 'dark' : ''}`}>
-      <main className='dark:bg-gray-900 px-10'>
+      <main className='dark:bg-gray-900 px-10 h-screen'>
         <section>
           <Navbar darkMode={ darkMode } setDarkMode={ setDarkMode }/>
           <About />
           <Media />
         </section>
-        <section>
+        {/* <section>
         <h2 className="text-md pt-20 pb-6 text-teal-800 font-medium md:text-3xl text-center">My Works Gallery</h2>
           <Carousel />
-        </section>
+        </section> */}
         <Footer />
       </main>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer footer-center px-4 pt-16 pb-8 text-gray-600">
+    <footer className="footer footer-center text-xs xs:text-xl px-4 pt-16 pb-8 text-gray-600 absolute inset-x-0 bottom-0">
       <aside>
         <p>Copyright © 2023 - All right reserved by Husnul Nawafil</p>
       </aside>
